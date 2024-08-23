11b4a5f02fe034faa580038676db99a329165061
Watson <watson0x90@gmail.com>
2024-08-23 11:06:52 -0600

    Update validate_accounts.py
    allows for passing single account id

--- reconnaissance/validate_accounts.py
+++ reconnaissance/validate_accounts.py
@@ -7,22 +7,6 @@ import requests
 import validators
 
 
-def main(args):
-    all_results = []
-    for line in args.input_file.readlines():
-        line = line.strip()
-        if(line and not line.startswith('#')):
-            result = validate_account('signin', line)
-            all_results.append(result)
-            if result['exists']:
-                print(json.dumps(result, indent=2))
-    args.input_file.close()
-
-    if(args.output_file is not None):
-        args.output_file.write(json.dumps(all_results, indent=2))
-        args.output_file.close()
-            
-
 def validate_account(validation_type, account):
     validation_functions = {
         'signin': validate_account_signin
@@ -57,16 +41,49 @@ def validate_account_signin(account):
         result['error'] = e
 
     return result
+
+def main(args):
+
+    all_results = []
+    result = None
+
+    if(args.account_id is not None):
+        result = validate_account('signin', args.account_id)
+        print(json.dumps(result, indent=2))
+        return
+    
+    else:
+        
+        for line in args.input_file.readlines():
+            line = line.strip()
+            if(line and not line.startswith('#')):
+                result = validate_account('signin', line)
+                all_results.append(result)
+                if result['exists']:
+                    print(json.dumps(result, indent=2))
+        args.input_file.close()
+
+    if(args.output_file is not None):
+        args.output_file.write(json.dumps(all_results, indent=2))
+        args.output_file.close()
+    else:
+        print(json.dumps(all_results, indent=2))
+
     
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Validate existence of an account ID or account alias.")
+    parser = argparse.ArgumentParser(description="Validate existence of an account ID or account alias.")    
     parser.add_argument('-i',
                         '--input-file',
                         type=argparse.FileType('r'),
-                        required=True)
+                        required=False)
+    parser.add_argument('-a',
+                        '--account-id',
+                        required=False)
     parser.add_argument('-o',
                         '--output-file',
                         type=argparse.FileType('w'))
     args = parser.parse_args()
     main(args)
+    
